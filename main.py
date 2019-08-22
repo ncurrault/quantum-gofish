8d02669a15fe8ba02912e5f6a311b1bb3d6c0f0b
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-08-21 17:47:56 -0700

    expanded game state logic, added player registration commands

--- main.py
+++ main.py
@@ -89,7 +89,11 @@ class GameState:
     def has_hand_size(player, n):
         self.hand_sizes[player] = n
 
-    def deduce_from_self():
+    def can_have(player, suit, n):
+        return n >= self.player_minimums[player][suit] and \
+            n <= self.player_maximums[player][suit]
+
+    def deduce_maxima():
         # there are exactly NUM_PER_SUIT cards in every suit
         for suit in range(self.num_players):
             for player in range(self.num_players):
@@ -101,13 +105,26 @@ class GameState:
                 self.has_at_most(player, suit, NUM_PER_SUIT - in_other_hands)
 
         # each player has the number of cards that they have
-        # TODO
+        for player in range(self.num_players):
+            for suit in range(self.num_players):
+                num_known_cards = 0
+                for other_suit in range(self.num_players):
+                    if suit == other_suit:
+                        continue
+                    num_known_cards += self.player_minimums[player][other_suit]
+                self.has_at_most(player, suit, self.hand_sizes[player] - num_known_cards)
+
+    def is_converged():
+        return self.player_maximums == self.player_minimums
 
     # external actions should call these, returning false if action is invalid
 
     def asked_for(player, suit):
-        pass
-        # err if they must have 0
+        if not self.can_have(player, suit, 1):
+            return False
+
+        self.has_at_least(player, suit, 1)
+        return True
 
     def gave_away(player, suit, n):
         pass
@@ -131,6 +148,16 @@ def list_player_handler(bot, update, chat_data=None):
 def whois_handler(bot, update, chat_data=None):
     pass
 
+def join_handler(bot, update, user_data=None, chat_data=None):
+    pass
+
+def leave_handler(bot, update, user_data=None, chat_data=None):
+    pass
+
+def start_game_handler(bot, update, user_data=None, chat_data=None):
+    pass
+
+
 def ask_handler(bot, update, user_data=None, chat_data=None, args=None):
     pass
 
@@ -148,10 +175,16 @@ if __name__ == "__main__":
     dispatcher.add_handler(get_static_handler("help"))
     dispatcher.add_handler(CommandHandler('feedback', feedback_handler, pass_args=True))
 
-    dispatcher.add_handler(CommandHandler('iam', i_am_handler, pass_user_data=True))
+    # TODO these 2 commands shouldn't be necessary: you should be able to just start a game, then whoever asks first
+    # is in it, as well as whoever they ask. but GameState currently does not support this
+    dispatcher.add_handler(CommandHandler('joingame', join_handler, pass_user_data=True, pass_chat_data=True))
+    dispatcher.add_handler(CommandHandler('leavegame', leave_handler, pass_user_data=True, pass_chat_data=True))
+
+    dispatcher.add_handler(CommandHandler('startgame', start_game_handler, pass_user_data=True, pass_chat_data=True))
 
     dispatcher.add_handler(CommandHandler('listplayers', list_player_handler, pass_chat_data=True))
     dispatcher.add_handler(CommandHandler('whois', whois_handler, pass_chat_data=True))
+    dispatcher.add_handler(CommandHandler('iam', i_am_handler, pass_user_data=True))
 
     dispatcher.add_handler(CommandHandler('ask', ask_handler, pass_args=True, pass_user_data=True, pass_chat_data=True))
     dispatcher.add_handler(CommandHandler('ihave', have_handler, pass_args=True, pass_user_data=True, pass_chat_data=True))
@@ -159,11 +192,10 @@ if __name__ == "__main__":
 
     dispatcher.add_error_handler(handle_error)
 
-    # allows viewing of exceptions
     logging.basicConfig(
         filename="ignore/bot.log",
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO) # not sure exactly how this works
+        level=logging.INFO)
 
     updater.start_polling()
     updater.idle()
